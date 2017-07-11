1a13885949c81e1c3978ed4d4013522f75c9b3bf
Hack Reactor Student <student@hackreactor.com>
2017-07-11 10:28:11 -0700

    Fix a bug. Now calls setPosition

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -8,8 +8,8 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
 
 };
 
-//BlinkyDancer.prototype = Object.create(Dancer.prototype);
-//BlinkyDancer.prototype.constructor = BlinkyDancer;
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
 BlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   // Dancer.prototype.step.call(this);
--- src/dancer.js
+++ src/dancer.js
@@ -1,8 +1,8 @@
 // Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps) {
-  this.top = top;
-  this.left = left;
-  this.timeBetweenSteps = timeBetweenSteps;
+  // this.top = top;
+  // this.left = left;
+  // this.timeBetweenSteps = timeBetweenSteps;
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
@@ -10,7 +10,7 @@ var Dancer = function(top, left, timeBetweenSteps) {
   this.step();
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  //this.setPosition(top, left);
+  this.setPosition(top, left);
 
 };
 
